fd995b5af0238efa8e2d27ff6fadd0451aab6f34
Vladimir Shimansky <pat@ajabu.ru>
2017-08-05 20:59:31 +0500

    move trains through temporary stations

--- control.lua
+++ control.lua
@@ -1,96 +1,125 @@
-script.on_event(
-    {defines.events.on_tick},
-    function(e)
-        UpdateTrainPaths()
-    end
-)
-
-script.on_event(
-    {defines.events.on_train_changed_state},
-    function(e)
-        UpdateTrainPaths()
+script.on_load(
+    function()
+        script.on_event(
+            {defines.events.on_tick},
+            function(e)
+                if e.tick % 10 == 0 then
+                    UpdateTrainPaths()
+                end
+            end
+        )
     end
 )
 
 function UpdateTrainPaths()
+    global.temporaryStations = global.temporaryStations or {}
+    global.actualStations = global.actualStations or {}
     local stations = {}
-    local stuckTrains = {}
+
+    for _, temporary in pairs(global.temporaryStations) do
+        if temporary.actualEntity.valid then
+            GetOrCreateStation(stations, temporary.actualEntity)
+        else
+            global.temporaryStations[_] = nil
+            global.actualStations[temporary.actualEntityUnitNumber] = nil
+            temporary.temporaryEntity.destroy()
+        end
+    end
 
     for _, force in pairs(game.forces) do
         for _, train in pairs(force.get_trains()) do
+            RemoveTemporaryFromSchedule(train, train.manual_mode)
             if train.manual_mode == false then
-                local entity = train.path_end_stop
+                local entity = train.path_end_stop or train.station
                 if entity then
                     local station = GetOrCreateStation(stations, entity)
                     table.insert(station.trains, train)
-                else
-                    table.insert(stuckTrains, train)
                 end
             end
         end
     end
 
     for _, station in pairs(stations) do
-        CheckOverload(station)
-    end
-
-    for _, station in pairs(stations) do
-        local index = station.maxTrains + 1
-        while #station.trains >= index do
-            local train = table.remove(station.trains, index)
-            RecalculatePath(train, stations)
-        end
-    end
-
-    for _, train in pairs(stuckTrains) do
-        RecalculatePath(train, stations)
-    end
-
-    for _, station in pairs(stations) do
-        if station.entity.backer_name == "" then
-            station.entity.backer_name = station.name
-        end
-    end
-end
-
-function SetTrainState(train, enable)
-    local schedule = train.schedule
-    if schedule then
-        if enable == false then
-            if #schedule.records > 1 then
-                schedule.current = schedule.current + 1
+        if CheckOverload(station) then
+            local trainsToTemporary = {}
+            local trainsToActual = {}
+            for _, train in pairs(station.trains) do
+                if
+                    (train.path_end_stop or train.station).unit_number ==
+                        station.entity.unit_number
+                 then
+                    table.insert(trainsToActual, train)
+                else
+                    table.insert(trainsToTemporary, train)
+                end
             end
-            local current = (schedule.current > #schedule.records) and 1 or schedule.current
-            train.schedule = {current = current, records = schedule.records}
+            if #trainsToTemporary > station.maxTrains then
+                for index = station.maxTrains + 1, #trainsToTemporary do
+                    train = trainsToTemporary[index]
+                    RemoveCurrentFromSchedule(train)
+                end
+            elseif #trainsToTemporary < station.maxTrains then
+                for index = 1, station.maxTrains - #trainsToTemporary do
+                    train = trainsToActual[index]
+                    AddTemporaryToSchedule(train, station.temporaryEntity.backer_name)
+                end
+            end
         end
     end
 end
 
-function RecalculatePath(train, stations)
-    if train.state == defines.train_state.arrive_signal then
-        return
-    end
-    local entity = train.path_end_stop
-    SetTrainState(train, true)
-    train.recalculate_path(true)
-    local newEntity = train.path_end_stop
-    if newEntity ~= entity and newEntity ~= nil then
-        local newStation = GetOrCreateStation(stations, newEntity)
-        table.insert(newStation.trains, train)
-        if CheckOverload(newStation) then
-            RecalculatePath(train, stations)
+function AddTemporaryToSchedule(train, name)
+    local records = train.schedule.records
+    local current = train.schedule.current
+    table.insert(
+        records,
+        current,
+        {
+            station = name,
+            wait_conditions = records[current].wait_conditions
+        }
+    )
+    train.schedule = {records = records, current = current}
+end
+
+function RemoveCurrentFromSchedule(train)
+    local records = train.schedule.records
+    table.remove(records, train.schedule.current)
+    train.schedule = {records = records, current = train.schedule.current}
+end
+
+function RemoveTemporaryFromSchedule(train, removeCurrent)
+    local records = train.schedule.records
+    local current = train.schedule.current
+    for index = #records, 1, -1 do
+        if
+            (removeCurrent or index ~= current) and
+                records[index].station:find("⇣") ~= nil
+         then
+            table.remove(records, index)
+            if current > index then
+                current = current - 1
+            end
         end
-    else
-        SetTrainState(train, false)
     end
+    train.schedule = {records = records, current = current}
 end
 
 function GetOrCreateStation(stations, entity)
     local station = stations[entity.unit_number]
 
     if station == nil then
+        local temporary = global.temporaryStations[entity.unit_number]
+        if temporary ~= nil then
+            entity = temporary.actualEntity
+            station = stations[entity.unit_number]
+        end
+    end
+
+    if station == nil then
         station = {
             entity = entity,
+            temporaryEntity = nil,
             maxTrains = GetMaxTrains(entity),
             trains = {},
             name = entity.backer_name
@@ -102,33 +131,69 @@ function GetOrCreateStation(stations, entity)
 end
 
 function CheckOverload(station)
-    if station.entity.backer_name ~= "" and #station.trains >= station.maxTrains then
-        station.entity.backer_name = ""
+    if station.maxTrains ~= nil then
+        if
+            global.actualStations[station.entity.unit_number] == nil and
+                #station.trains > 0 and
+                #station.trains >= station.maxTrains
+         then
+            local temporaryEntity =
+                station.entity.surface.create_entity {
+                name = "ctr-temporary-stop",
+                position = station.entity.position,
+                direction = station.entity.direction,
+                force = station.entity.force
+            }
+            temporaryEntity.backer_name = "⇣" .. station.entity.unit_number
+            local temporary = {
+                actualEntityUnitNumber = station.entity.unit_number,
+                actualEntity = station.entity,
+                temporaryEntity = temporaryEntity
+            }
+            global.actualStations[station.entity.unit_number] = temporary
+            global.temporaryStations[temporaryEntity.unit_number] = temporary
+        end
+
+        local circuit_condition = station.entity.get_control_behavior().circuit_condition
+        circuit_condition.condition.constant = #station.trains
+        station.entity.get_control_behavior().circuit_condition = circuit_condition
     end
-    return #station.trains > station.maxTrains
+
+    local temporary = global.actualStations[station.entity.unit_number]
+    if temporary ~= nil then
+        if #station.trains == 0 then
+            global.temporaryStations[temporary.temporaryEntity.unit_number] = nil
+            global.actualStations[temporary.actualEntityUnitNumber] = nil
+            temporary.temporaryEntity.destroy()
+        else
+            station.temporaryEntity = temporary.temporaryEntity
+        end
+    end
+
+    return station.maxTrains ~= nil and #station.trains >= station.maxTrains
 end
 
 function GetMaxTrains(entity)
     local maxTrains = nil
     local behavior = entity.get_control_behavior()
     if
-        behavior and
-            behavior.enable_disable and
+        behavior and behavior.enable_disable and
             behavior.circuit_condition.condition.comparator == ">" and
             behavior.circuit_condition.condition.first_signal and
             behavior.circuit_condition.condition.first_signal.name == "locomotive" and
-            behavior.circuit_condition.condition.second_signal == nil and
-            behavior.circuit_condition.condition.constant == 0
+            behavior.circuit_condition.condition.second_signal == nil
      then
+        maxTrains = 0
         for w = 2, 3 do
             local network = entity.get_circuit_network(w)
             if network then
-                local signal = network.get_signal({["name"] = "locomotive", ["type"] = "item"})
+                local signal =
+                    network.get_signal({["name"] = "locomotive", ["type"] = "item"})
                 if signal and signal ~= 0 then
-                    maxTrains = (maxTrains or 0) + signal
+                    maxTrains = maxTrains + signal
                 end
             end
         end
     end
-    return math.max(maxTrains or 999999999, 0)
+    return maxTrains
 end
---
+++ prototypes/entities.lua
@@ -0,0 +1,20 @@
+local temporaryStop = table.deepcopy(data.raw["train-stop"]["train-stop"])
+temporaryStop.name = "ctr-temporary-stop"
+temporaryStop.collision_box = {{0.0, 0.0}, {0.0, 0.0}}
+temporaryStop.selection_box = {{0.0, 0.0}, {0.0, 0.0}}
+temporaryStop.collision_mask = {"resource-layer"}
+temporaryStop.flags = {"not-blueprintable", "not-deconstructable"}
+temporaryStop.animations = nil
+temporaryStop.light1 = nil
+temporaryStop.light2 = nil
+temporaryStop.rail_overlay_animations = nil
+temporaryStop.top_animations = nil
+
+local temporaryStopControl = table.deepcopy(data.raw["item"]["train-stop"])
+temporaryStopControl.name = "ctr-temporary-stop"
+temporaryStopControl.place_result = "ctr-temporary-stop"
+
+data:extend {
+    temporaryStop,
+    temporaryStopControl
+}
